a4d4c57b082ff03d3d25369e1dcc10f22df48e2a
cacryingkabbage <167156462+cacryingkabbage@users.noreply.github.com>
2025-12-06 22:15:34 +0800

    Update admin.js

--- mongo-crud/admin.js
+++ mongo-crud/admin.js
@@ -1,1 +1,21 @@
+import Order from "../models/orders.js";
+import Product from "../models/products.js";
+import Customer from "../models/customer.js";
+import catchAsync from "../utils/catchAsync.js";
 
+export const getDashboardStats = catchAsync(async (req, res) => {
+  const [orderCount, productCount, customerCount] = await Promise.all([
+    Order.countDocuments(),
+    Product.countDocuments(),
+    Customer.countDocuments(),
+  ]);
+
+  res.json({
+    success: true,
+    stats: {
+      orders: orderCount,
+      products: productCount,
+      customers: customerCount,
+    },
+  });
+});
