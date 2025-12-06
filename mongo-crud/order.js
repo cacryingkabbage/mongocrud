fe6171f373ace743135b542c55ec6d18dee87120
cacryingkabbage <167156462+cacryingkabbage@users.noreply.github.com>
2025-12-06 22:17:50 +0800

    Update order.js

--- mongo-crud/order.js
+++ mongo-crud/order.js
@@ -1,1 +1,39 @@
+import Order from "../models/orders.js";
+import catchAsync from "../utils/catchAsync.js";
 
+export const createOrder = catchAsync(async (req, res) => {
+  const orderData = {
+    ...req.body,
+    customer_id: req.user._id,
+  };
+  const order = await Order.create(orderData);
+  res.status(201).json({ success: true, order });
+});
+
+export const getOrders = catchAsync(async (req, res) => {
+  const orders = await Order.find()
+    .populate("customer_id")
+    .populate("order_date");
+  res.json({ success: true, orders });
+});
+
+export const getOrder = catchAsync(async (req, res) => {
+  const order = await Order.findById(req.params.id)
+    .populate("customer_id")
+    .populate("order_date");
+  if (!order) return res.status(404).json({ message: "Order not found" });
+  res.json({ success: true, order });
+});
+
+export const updateOrder = catchAsync(async (req, res) => {
+  const order = await Order.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  if (!order) return res.status(404).json({ message: "Order not found" });
+  res.json({ success: true, message: "Order updated", order });
+});
+
+export const deleteOrder = catchAsync(async (req, res) => {
+  await Order.findByIdAndDelete(req.params.id);
+  res.json({ success: true, message: "Order deleted" });
+});
