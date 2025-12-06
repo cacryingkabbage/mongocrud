6d0fc0b1c93e028a1137270ea7f42a36ba444ad4
cacryingkabbage <167156462+cacryingkabbage@users.noreply.github.com>
2025-12-06 22:19:20 +0800

    Update user.js

--- mongo-crud/user.js
+++ mongo-crud/user.js
@@ -1,1 +1,53 @@
+const User = require("../models/User");
 
+// CREATE USER
+exports.createUser = async (req, res) => {
+  try {
+    const newUser = await User.create(req.body);
+    res.json(newUser);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+};
+
+// GET ALL USERS
+exports.getUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+};
+
+// GET ONE USER
+exports.getUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    res.json(user);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+};
+
+// UPDATE USER
+exports.updateUser = async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.json(user);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+};
+
+// DELETE USER
+exports.deleteUser = async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.json({ message: "User deleted" });
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+};
